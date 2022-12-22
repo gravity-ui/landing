a4ae09fa1dc66bd3c0a678769c0743646d149899
Maksim Sitnikov <imsitnikov@ya.ru>
2022-12-22 16:34:03 +0300

    Fixes in styles (#14)
    Co-authored-by: Maksim Sitnikov <msitnikov1@yandex-team.ru>

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {hydrate, render} from 'react-dom';
 import {App} from './components/App/App';
-import './index.scss';
+import './styles.scss';
+import './vendors.scss';
 
 const rootElement = document.getElementById('root');
 
