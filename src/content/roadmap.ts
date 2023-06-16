1f047ac503261b34e11a4c7e4564bc8a6a9a9b20
Daniil Gaponov <dagaponov@yandex-team.ru>
2023-06-16 12:30:51 +0300

    feat: update roadmap events

--- src/content/roadmap.ts
+++ src/content/roadmap.ts
@@ -2,15 +2,11 @@ import {RoadmapEvent, RoadmapEventStatus} from '../components/Roadmap';
 
 export const roadmapEvents: RoadmapEvent[] = [
     {
-        title: 'Library for rendering neat and functional forms described by JSON schema',
+        title: 'А library for rendering neat and functional forms described by JSON schema',
         status: RoadmapEventStatus.COMPLETED,
     },
     {
-        title: 'A library with a wide range of easy-to-use components for creating interfaces of any complexity.',
-        status: RoadmapEventStatus.COMPLETED,
-    },
-    {
-        title: 'Add components to select date in the Ui-kit library',
+        title: 'A library with a wide range of easy-to-use components for creating interfaces of any complexity',
         status: RoadmapEventStatus.COMPLETED,
     },
     {
@@ -18,11 +14,19 @@ export const roadmapEvents: RoadmapEvent[] = [
         status: RoadmapEventStatus.IN_PROGRESS,
     },
     {
-        title: '<b>Date-components:</b> new library for working with date and time.',
-        status: RoadmapEventStatus.PLANNED,
+        title: '<b>Date-components:</b> new library for working with date and time',
+        status: RoadmapEventStatus.IN_PROGRESS,
     },
     {
         title: '<b>UIKit:</b> Extended style customization options and themes support',
+        status: RoadmapEventStatus.PLANNED,
+    },
+    {
+        title: '<b>Docs:</b> Improved components documentation',
+        status: RoadmapEventStatus.PLANNED,
+    },
+    {
+        title: '<b>Design:</b> Public Figma templates and guidelines',
         status: RoadmapEventStatus.PLANNED,
     },
 ];
