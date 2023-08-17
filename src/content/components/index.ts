b5bd991065c467393cd67ed070ef3ce42b835fc3
Maksim Sitnikov <msitnikov1@yandex-team.com>
2023-08-17 16:34:06 +0300

    hide components lib

--- src/content/components/index.ts
+++ src/content/components/index.ts
@@ -1,7 +1,7 @@
-import {components} from './components';
+// import {components} from './components';
 import {Lib} from './types';
 import {uikit} from './uikit';
 
 export type {SandboxType, SandboxProps, Component} from './types';
 
-export const libs: Lib[] = [uikit, components];
+export const libs: Lib[] = [uikit /*, components*/];
