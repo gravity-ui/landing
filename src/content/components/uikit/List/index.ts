2d041215b83eedb05fc6fdb7d9afe61b4be8629f
Juli Ovechkina <berdysheva@yandex-team.ru>
2023-08-25 15:23:37 +0100

    docs: add list (#75)

---
+++ src/content/components/uikit/List/index.ts
@@ -0,0 +1,35 @@
+import dynamic from 'next/dynamic';
+import {Repos} from '../../../../types/common';
+import {getGithubUrl, getReadmeUrl} from '../../utils';
+
+const getterOptions = {repoName: Repos.Uikit, componentName: 'List'};
+
+export const listConfig = {
+    id: 'list',
+    title: 'List',
+    githubUrl: getGithubUrl(getterOptions),
+    content: {
+        readmeUrl: getReadmeUrl(getterOptions),
+    },
+    sandbox: {
+        component: dynamic(() => import('./ListComponent').then((mod) => mod.ListComponent)),
+        props: {
+            filterable: {
+                type: 'switch',
+                defaultValue: true,
+            },
+            sortable: {
+                type: 'switch',
+                defaultValue: false,
+            },
+            filterPlaceholder: {
+                type: 'input',
+                defaultValue: 'Default placeholder',
+            },
+            emptyPlaceholder: {
+                type: 'input',
+                defaultValue: 'Not found',
+            },
+        },
+    },
+};
