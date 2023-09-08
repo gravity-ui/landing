57497e2ce2c605b296dbb8aad92bd57daa9b80c2
Anton Vikulov <rndnm@yandex-team.ru>
2023-09-08 18:09:40 +0500

    feat: add docs for Breadcrumbs

---
+++ src/content/components/uikit/Breadcrumbs/index.ts
@@ -0,0 +1,50 @@
+import dynamic from 'next/dynamic';
+
+import {Repos} from '../../../../types/common';
+import {getGithubUrl, getReadmeUrl} from '../../utils';
+
+const getterOptions = {repoName: Repos.Uikit, componentName: 'Breadcrumbs'};
+
+export const breadcrumbsConfig = {
+    id: 'breadcrumbs',
+    title: 'Breadcrumbs',
+    githubUrl: getGithubUrl(getterOptions),
+    content: {
+        readmeUrl: getReadmeUrl(getterOptions),
+    },
+    sandbox: {
+        component: dynamic(() =>
+            import('./BreadcrumbsWrapper').then((mod) => mod.BreadcrumbsWrapper),
+        ),
+        props: {
+            firstDisplayedItemsCount: {
+                type: 'radioButton',
+                values: [
+                    {
+                        value: 0,
+                        content: '0',
+                    },
+                    {
+                        value: 1,
+                        content: '1',
+                    },
+                ],
+                defaultValue: 0,
+            },
+            lastDisplayedItemsCount: {
+                type: 'radioButton',
+                values: [
+                    {
+                        value: 1,
+                        content: '1',
+                    },
+                    {
+                        value: 2,
+                        content: '2',
+                    },
+                ],
+                defaultValue: 1,
+            },
+        },
+    },
+};
