4882d723c76658803630bb42267299ea34a0b67a
Daniil Gaponov <dagaponov@yandex-team.ru>
2023-09-20 13:54:20 +0300

    feat: apply global theme and color styles

--- src/components/Interactive/components/InteractiveCard/InteractiveCard.tsx
+++ src/components/Interactive/components/InteractiveCard/InteractiveCard.tsx
@@ -1,7 +1,8 @@
-import {ThemeProvider} from '@gravity-ui/uikit';
-import React, {useCallback, useState} from 'react';
+import {Theme, ThemeProvider} from '@gravity-ui/uikit';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import {block} from '../../../../utils';
+import {useInteractiveContext} from '../../InteractiveContext';
 
 import './InteractiveCard.scss';
 
@@ -9,27 +10,32 @@ const b = block('interactive-card');
 
 interface InteractiveCardProps {
     children: React.ReactNode;
-    onFlip?: () => void;
 }
 
-export const InteractiveCard: React.FC<InteractiveCardProps> = ({onFlip, children}) => {
-    const [isFlipped, setFlipped] = useState(false);
+export const InteractiveCard: React.FC<InteractiveCardProps> = ({children}) => {
+    const {color, theme: globalTheme} = useInteractiveContext();
+    const [localTheme, setLocalTheme] = useState<Theme>('dark');
+
+    useEffect(() => {
+        setLocalTheme(globalTheme);
+    }, [globalTheme]);
 
     const flip = useCallback(() => {
-        setFlipped((prev) => !prev);
-        onFlip?.();
-    }, [onFlip]);
+        setLocalTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+    }, []);
+
+    const isFlipped = localTheme === 'light';
 
     return (
         <div className={b({flipped: isFlipped})} onClick={flip}>
             <div className={b('body')}>
                 <div className={b('side', {front: true})}>
-                    <ThemeProvider theme="dark" rootClassName={b('theme-wrapper')} scoped>
+                    <ThemeProvider theme="dark" rootClassName={b('theme-wrapper', {color})} scoped>
                         <div className={b('card')}>{children}</div>
                     </ThemeProvider>
                 </div>
                 <div className={b('side', {back: true})}>
-                    <ThemeProvider theme="light" rootClassName={b('theme-wrapper')} scoped>
+                    <ThemeProvider theme="light" rootClassName={b('theme-wrapper', {color})} scoped>
                         <div className={b('card')}>{children}</div>
                     </ThemeProvider>
                 </div>
