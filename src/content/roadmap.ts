9a3a2fcf656904d8ecd050e144fa8c3cab3e9736
vvtimofeev <108340247+vvtimofeev@users.noreply.github.com>
2023-09-25 10:50:52 +0300

    roadmap change (#118)
    * feat: changes in roadmap
    
    * feat: roadmap correction
    
    * feat: change in a11y news

--- src/content/roadmap.ts
+++ src/content/roadmap.ts
@@ -2,35 +2,31 @@ import {RoadmapTask, RoadmapTaskStatus} from '../components/Roadmap';
 
 export const roadmapTasks: RoadmapTask[] = [
     {
-        title: '<b>UIKit:</b> library with a wide range of easy-to-use components for creating interfaces of any complexity',
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
-        title: '<b>Dynamic Forms:</b> library for rendering neat and functional forms described by JSON schema',
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
-        title: '<b>Icons:</b> new section of Gravity site with search and preview',
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
         title: '<b>Design:</b> Public Figma templates',
         status: RoadmapTaskStatus.Completed,
     },
     {
-        title: '<b>Date Components:</b> new library for working with date and time',
-        status: RoadmapTaskStatus.InProgress,
-    },
-    {
         title: '<b>UIKit:</b> Extended style customization options and themes support',
-        status: RoadmapTaskStatus.InProgress,
+        status: RoadmapTaskStatus.Completed,
     },
     {
         title: '<b>Docs:</b> Improved components documentation',
-        status: RoadmapTaskStatus.InProgress,
+        status: RoadmapTaskStatus.Completed,
     },
     {
         title: '<b>Design:</b> Guidelines',
+        status: RoadmapTaskStatus.Completed,
+    },
+    {
+        title: '<b>Date Components:</b> New library for working with date and time',
+        status: RoadmapTaskStatus.InProgress,
+    },
+    {
+        title: '<b>UIKit:</b> Right-to-left languages support',
+        status: RoadmapTaskStatus.InProgress,
+    },
+    {
+        title: '<b>UIKit:</b> improvements in digital accessibility',
         status: RoadmapTaskStatus.InProgress,
     },
 ];
