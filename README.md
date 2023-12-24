27b7a448c63053e67c7e0c0ebd3c7a26b96d717e
Maksim Sitnikov <msitnikov1@yandex-team.com>
2023-12-24 14:20:11 +0300

    feat: Add dev branches for testing component documentation

--- README.md
+++ README.md
@@ -15,4 +15,9 @@ npm run start
 # .env file
 
 GITHUB_PROFILE="your profile" # default gravity-ui, target profile for pulling documentation from components
+
+# Dev branches for testing component documentation
+DEV_BRANCH_UIKIT="dev-branch-name"
+DEV_BRANCH_COMPONENTS="dev-branch-name"
+DEV_BRANCH_DATE_COMPONENTS="dev-branch-name"
 ```
--- src/content/components/utils.ts
+++ src/content/components/utils.ts
@@ -5,9 +5,10 @@ import {Repos} from '../../types/common';
 const githubTargetProfile = process.env.GITHUB_PROFILE || TARGET_PROFILE;
 
 const TARGET_REPOS_VERSIONS = {
-    [Repos.Uikit]: packagesVersions[Repos.Uikit],
-    [Repos.Components]: packagesVersions[Repos.Components],
-    // [Repos.DateComponents]: packagesVersions['@gravity-ui/date-components'],
+    [Repos.Uikit]: process.env.DEV_BRANCH_UIKIT || `v${packagesVersions[Repos.Uikit]}`,
+    [Repos.Components]:
+        process.env.DEV_BRANCH_COMPONENTS || `v${packagesVersions[Repos.Components]}`,
+    // [Repos.DateComponents]: process.env.DEV_BRANCH_DATE_COMPONENTS || `v${packagesVersions['@gravity-ui/date-components']}`,
 };
 
 export type GetterProps = {componentName: string; repoName: Repos};
@@ -21,7 +22,7 @@ export const mappingOptions = (arr: string[]) =>
     }));
 
 export const getReadmeUrl: RepoInfoGetterFunc = ({componentName, repoName}) =>
-    `https://raw.githubusercontent.com/${githubTargetProfile}/${repoName}/v${TARGET_REPOS_VERSIONS[repoName]}/src/components/${componentName}/README.md`;
+    `https://raw.githubusercontent.com/${githubTargetProfile}/${repoName}/${TARGET_REPOS_VERSIONS[repoName]}/src/components/${componentName}/README.md`;
 
 export const getGithubUrl: RepoInfoGetterFunc = ({componentName, repoName}) =>
     `https://github.com/${githubTargetProfile}/${repoName}/tree/main/src/components/${componentName}`;
