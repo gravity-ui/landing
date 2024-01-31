ad955af329a7e20896126ca7cff5147c10a12ea6
vvtimofeev <108340247+vvtimofeev@users.noreply.github.com>
2024-01-31 16:33:03 +0300

    feat: changed roadmap for 2024 (#164)

--- src/content/roadmap.ts
+++ src/content/roadmap.ts
@@ -2,24 +2,8 @@ import {RoadmapTask, RoadmapTaskStatus} from '../components/Roadmap';
 
 export const roadmapTasks: RoadmapTask[] = [
     {
-        title: '<b>Design:</b> Public Figma templates',
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
-        title: '<b>UIKit:</b> Extended style customization options and themes support',
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
-        title: '<b>Docs:</b> Improved components documentation',
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
-        title: '<b>Design:</b> Guidelines',
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
         title: '<b>Date Components:</b> New library for working with date and time',
-        status: RoadmapTaskStatus.InProgress,
+        status: RoadmapTaskStatus.Completed,
     },
     {
         title: '<b>UIKit:</b> Right-to-left languages support',
@@ -28,5 +12,37 @@ export const roadmapTasks: RoadmapTask[] = [
     {
         title: '<b>UIKit:</b> improvements in digital accessibility',
         status: RoadmapTaskStatus.InProgress,
+    },
+    {
+        title: '<b>Figma:</b> color theme template builder',
+        status: RoadmapTaskStatus.InProgress,
+    },
+    {
+        title: '<b>Development farm</b>',
+        status: RoadmapTaskStatus.InProgress,
+    },
+    {
+        title: '<b>Docs:</b> Date Components in Components section',
+        status: RoadmapTaskStatus.InProgress,
+    },
+    {
+        title: '<b>UIKit:</b> emoji picker, color picker, label input and range input components',
+        status: RoadmapTaskStatus.InProgress,
+    },
+    {
+        title: '<b>UIKit:</b> Migration to Floating UI',
+        status: RoadmapTaskStatus.Planned,
+    },
+    {
+        title: '<b>Docs:</b> Page constructor in Components section',
+        status: RoadmapTaskStatus.Planned,
+    },
+    {
+        title: '<b>UIKit:</b> Extended mobile support',
+        status: RoadmapTaskStatus.Planned,
+    },
+    {
+        title: '<b>Page constructor:</b> footer block',
+        status: RoadmapTaskStatus.Planned,
     },
 ];
