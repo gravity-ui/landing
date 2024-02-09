5a51b9ff859b2a2960e98984e5c9b4353815f119
Kirill Revenkov <revenkov-k@yandex-team.ru>
2024-02-09 16:38:10 +0300

    feat: add sandbox for RelativeDateField

--- src/content/components/date-components/RelativeDateField/index.ts
+++ src/content/components/date-components/RelativeDateField/index.ts
@@ -1,7 +1,86 @@
+import dynamic from 'next/dynamic';
+import {Repos} from 'src/types/common';
+
 import {Component} from '../../types';
+import {getGithubUrl, getReadmeUrl, mappingOptions} from '../../utils';
+
+const getterOptions = {repoName: Repos.DateComponents, componentName: 'RelativeDateField'};
 
 export const relativeDateFieldConfig: Component = {
     id: 'relative-date-field',
     title: 'Relative Date Field',
-    isComingSoon: true,
+    githubUrl: getGithubUrl(getterOptions),
+    content: {
+        readmeUrl: getReadmeUrl(getterOptions),
+    },
+    sandbox: {
+        component: dynamic(() =>
+            import('@gravity-ui/date-components').then((mod) => mod.RelativeDateField),
+        ),
+        props: {
+            view: {
+                type: 'radioButton',
+                values: mappingOptions(['normal', 'clear']),
+                defaultValue: 'normal',
+            },
+            size: {
+                type: 'radioButton',
+                values: mappingOptions(['s', 'm', 'l', 'xl']),
+                defaultValue: 'm',
+            },
+            placeholder: {
+                type: 'input',
+                defaultValue: 'Type here...',
+            },
+            label: {
+                type: 'input',
+            },
+            validationState: {
+                type: 'radioButton',
+                values: mappingOptions(['normal', 'invalid']),
+                defaultValue: 'normal',
+            },
+            errorMessage: {
+                type: 'input',
+                defaultValue: 'Input is invalid',
+            },
+            disabled: {
+                type: 'switch',
+                defaultValue: false,
+            },
+            hasClear: {
+                type: 'switch',
+                defaultValue: false,
+            },
+            hasTime: {
+                type: 'switch',
+                defaultValue: false,
+            },
+            readOnly: {
+                type: 'switch',
+                defaultValue: false,
+            },
+            pin: {
+                type: 'select',
+                values: mappingOptions([
+                    'round-round',
+                    'brick-brick',
+                    'clear-clear',
+                    'round-brick',
+                    'brick-round',
+                    'round-clear',
+                    'clear-round',
+                    'brick-clear',
+                    'clear-brick',
+                ]),
+                defaultValue: 'round-round',
+            },
+            leftContent: {
+                type: 'input',
+            },
+            rightContent: {
+                type: 'input',
+            },
+        },
+    },
 };
