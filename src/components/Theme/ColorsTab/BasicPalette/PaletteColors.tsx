b9fb4089dfecfa3be68b64b0fb45501d8229bf1f
Daniil Gaponov <dagaponov@yandex-team.ru>
2024-06-10 00:20:14 +0300

    feat: CRUD for basic palette

--- src/components/Theme/ColorsTab/BasicPalette/PaletteColors.tsx
+++ src/components/Theme/ColorsTab/BasicPalette/PaletteColors.tsx
@@ -1,28 +1,104 @@
-import {Plus} from '@gravity-ui/icons';
-import {Button, Flex, Icon, Text} from '@gravity-ui/uikit';
+import {Plus, TrashBin} from '@gravity-ui/icons';
+import {Button, Flex, Icon, Text, TextInput, sp} from '@gravity-ui/uikit';
+import debounce from 'lodash/debounce';
 import React from 'react';
 
+import {block} from '../../../../utils';
 import {Palette} from '../../types';
+
+import './PaletteColors.scss';
+
+const b = block('palette-colors');
+
+interface PaletteColorEditorProps {
+    paletteColorData: Palette[0];
+    onUpdateTitle: (oldTitle: string, newTitle: string) => void;
+    onDelete: (title: string) => void;
+}
+
+const PaletteColorEditor: React.FC<PaletteColorEditorProps> = ({
+    onDelete,
+    onUpdateTitle,
+    paletteColorData,
+}) => {
+    const {title, isCustom} = paletteColorData;
+
+    const [localTitle, setLocalTitle] = React.useState(title);
+
+    React.useEffect(() => {
+        setLocalTitle(title);
+    }, [title]);
+
+    const handleDelete = React.useCallback(() => onDelete(title), [onDelete, title]);
+
+    const updateTitle = React.useCallback(
+        (newTitle: string) => onUpdateTitle(title, newTitle),
+        [title, onUpdateTitle],
+    );
+
+    const debouncedUpdateTitle = React.useMemo(() => debounce(updateTitle, 500), [updateTitle]);
+
+    const handleUpdateTitle = React.useCallback(
+        (newTitle: string) => {
+            setLocalTitle(newTitle);
+            debouncedUpdateTitle(newTitle);
+        },
+        [debouncedUpdateTitle],
+    );
+
+    if (!isCustom) {
+        return (
+            <div className={sp({py: 2})}>
+                <Text variant="body-2">{title}</Text>
+            </div>
+        );
+    }
+
+    return (
+        <Flex gap={2}>
+            <TextInput size="l" view="normal" value={localTitle} onUpdate={handleUpdateTitle} />
+            <Button size="l" onClick={handleDelete}>
+                <Icon data={TrashBin} size={16} />
+            </Button>
+        </Flex>
+    );
+};
 
 interface PaletteColorsProps {
     palette: Palette;
     onAddColorClick: () => void;
+    onDeleteColor: (title: string) => void;
+    onUpdateColorTitle: (oldTitle: string, newTitle: string) => void;
 }
 
-export const PaletteColors: React.FC<PaletteColorsProps> = ({palette, onAddColorClick}) => {
+export const PaletteColors: React.FC<PaletteColorsProps> = ({
+    palette,
+    onAddColorClick,
+    onDeleteColor,
+    onUpdateColorTitle,
+}) => {
     return (
-        <Flex direction="column" style={{paddingTop: '16px'}}>
-            <Text variant="subheader-3" style={{marginBottom: '22px'}}>
-                Support Colors for various cases and states
+        <Flex direction="column" className={b()} grow={true}>
+            <Text variant="subheader-3" className={b('title')}>
+                Support Colors for various cases and
+                <br /> states
             </Text>
-            <Flex gap={8} direction="column">
-                {palette.map(({title}) => (
-                    <Text variant="body-2" key={title}>
-                        {title}
-                    </Text>
+            <Flex direction="column" gap={4}>
+                {palette.map((paletteColorData) => (
+                    <PaletteColorEditor
+                        key={paletteColorData.title}
+                        paletteColorData={paletteColorData}
+                        onDelete={onDeleteColor}
+                        onUpdateTitle={onUpdateColorTitle}
+                    />
                 ))}
-                <Button onClick={onAddColorClick} view="outlined-action" size="xl">
-                    <Icon data={Plus} /> Add color
+                <Button
+                    className={b('add-button')}
+                    onClick={onAddColorClick}
+                    view="outlined-action"
+                    size="xl"
+                >
+                    <Icon data={Plus} size={20} /> Add color
                 </Button>
             </Flex>
         </Flex>
