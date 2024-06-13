6e632ad75b951a5076b246c4365cbb6f48db278e
Daniil Gaponov <dagaponov@yandex-team.ru>
2024-06-13 22:38:07 +0300

    feat: advanced mode switch

---
+++ src/components/Theme/ColorsTab/PrivateColorsSettings/PrivateColorsSettings.tsx
@@ -0,0 +1,78 @@
+import {FormRow} from '@gravity-ui/components';
+import {Flex, Text} from '@gravity-ui/uikit';
+import React from 'react';
+
+import {block} from '../../../../utils';
+import {PrivateColorSelect} from '../../../PrivateColorSelect/PrivateColorSelect';
+import {ThemePicker} from '../../../ThemePicker';
+import {useThemeColor} from '../../hooks/useThemeColor';
+import {useThemeCreator} from '../../hooks/useThemeCreator';
+import type {ColorsOptions, ThemeVariant} from '../../types';
+import {ThemeColorOption} from '../../utils';
+
+import './PrivateColorsSettings.scss';
+
+const b = block('private-colors-settings');
+
+interface PrivateColorEditorProps {
+    name: keyof ColorsOptions;
+    theme: ThemeVariant;
+    colorGroups: ThemeColorOption[];
+}
+
+const PrivateColorEditor: React.FC<PrivateColorEditorProps> = ({name, theme, colorGroups}) => {
+    const [color, setColor] = useThemeColor({
+        name,
+        theme,
+    });
+
+    return <PrivateColorSelect groups={colorGroups} value={color} onChange={setColor} />;
+};
+
+export type EditableColorOption = {
+    title: string;
+    name: keyof ColorsOptions;
+};
+
+interface PrivateColorsSettingsProps {
+    title: string;
+    options: EditableColorOption[];
+}
+
+export const PrivateColorsSettings: React.FC<PrivateColorsSettingsProps> = ({title, options}) => {
+    const [theme, setTheme] = React.useState<ThemeVariant>('light');
+    const {getThemePrivateColorOptions} = useThemeCreator();
+
+    const colorGroups = React.useMemo(
+        () => getThemePrivateColorOptions(theme),
+        [getThemePrivateColorOptions, theme],
+    );
+
+    return (
+        <Flex gap={8} direction="column" className={b()}>
+            <Flex>
+                <h1>{title}</h1>
+            </Flex>
+            <Flex gap={9}>
+                <Flex direction="column">
+                    <FormRow label={<Text variant="body-3">Theme</Text>} className={b('row')}>
+                        <ThemePicker value={theme} onUpdate={setTheme} />
+                    </FormRow>
+                    {options.map((option) => (
+                        <FormRow
+                            key={option.name}
+                            label={<Text variant="body-3">{option.title}</Text>}
+                            className={b('row')}
+                        >
+                            <PrivateColorEditor
+                                name={option.name}
+                                theme={theme}
+                                colorGroups={colorGroups}
+                            />
+                        </FormRow>
+                    ))}
+                </Flex>
+            </Flex>
+        </Flex>
+    );
+};
--- src/components/Theme/ColorsTab/index.tsx
+++ src/components/Theme/ColorsTab/index.tsx
@@ -2,17 +2,85 @@ import {Grid} from '@gravity-ui/page-constructor';
 import {Flex} from '@gravity-ui/uikit';
 import React from 'react';
 
-import {AdvancedBrandPalette} from './AdvancedBrandPalette/AdvancedBrandPalette';
 import {BasicPalette} from './BasicPalette/BasicPalette';
 import {MainSettings} from './MainSettings/MainSettings';
+import {
+    EditableColorOption,
+    PrivateColorsSettings,
+} from './PrivateColorsSettings/PrivateColorsSettings';
+
+const ADVANCED_COLORS_OPTIONS: EditableColorOption[] = [
+    {
+        title: 'Hovered Brand Color',
+        name: 'base-brand-hover',
+    },
+    {
+        title: 'Brand Text',
+        name: 'text-brand',
+    },
+    {
+        title: 'Higher Contrast Brand Text',
+        name: 'text-brand-contrast',
+    },
+    {
+        title: 'Brand Line Color',
+        name: 'line-brand',
+    },
+    {
+        title: 'Selection Background',
+        name: 'base-selection',
+    },
+    {
+        title: 'Hovered Selection Background',
+        name: 'base-selection-hover',
+    },
+];
+
+const ADDITIONAL_COLORS_OPTIONS: EditableColorOption[] = [
+    {
+        title: 'Link',
+        name: 'text-link',
+    },
+    {
+        title: 'Hovered Link',
+        name: 'text-link-hover',
+    },
+    {
+        title: 'Visited Link',
+        name: 'text-link-visited',
+    },
+    {
+        title: 'Hovered Visited Link',
+        name: 'text-link-visited-hover',
+    },
+];
 
 export const ColorsTab = () => {
+    const [advancedModeEnabled, toggleAdvancedMode] = React.useReducer(
+        (enabled) => !enabled,
+        false,
+    );
+
     return (
         <Grid>
             <Flex direction="column" gap={10}>
-                <MainSettings />
-                <BasicPalette />
-                <AdvancedBrandPalette />
+                <MainSettings
+                    advancedModeEnabled={advancedModeEnabled}
+                    toggleAdvancedMode={toggleAdvancedMode}
+                />
+                {advancedModeEnabled && (
+                    <React.Fragment>
+                        <BasicPalette />
+                        <PrivateColorsSettings
+                            title="Advanced Brand Palette"
+                            options={ADVANCED_COLORS_OPTIONS}
+                        />
+                        <PrivateColorsSettings
+                            title="Additional Colors"
+                            options={ADDITIONAL_COLORS_OPTIONS}
+                        />
+                    </React.Fragment>
+                )}
             </Flex>
         </Grid>
     );
