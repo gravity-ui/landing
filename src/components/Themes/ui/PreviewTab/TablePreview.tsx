978d529b394ece0fd3f3ebaee535b185d8ad9996
aobityutskiy <aobityutskiy@yandex-team.ru>
2024-06-24 16:37:21 +0300

    feat: add table and form preview

---
+++ src/components/Themes/ui/PreviewTab/TablePreview.tsx
@@ -0,0 +1,154 @@
+import {dateTime} from '@gravity-ui/date-utils';
+import {Copy, Pencil, Plus, TrashBin} from '@gravity-ui/icons';
+import {
+    Button,
+    Flex,
+    Icon,
+    Label,
+    LabelProps,
+    Pagination,
+    PaginationProps,
+    Select,
+    Table,
+    TableColumnConfig,
+    TableDataItem,
+    Text,
+    TextInput,
+    User,
+    withTableActions,
+    withTableSelection,
+    withTableSorting,
+} from '@gravity-ui/uikit';
+import {TableAction} from '@gravity-ui/uikit/build/esm/components/Table/hoc/withTableActions/withTableActions';
+import React from 'react';
+
+import avatar1Asset from '../../../../assets/avatar-1.png';
+
+import {b} from './PreviewTab';
+import './PreviewTab.scss';
+import {labels, projects, users} from './constants';
+
+const SelectionTable = withTableSelection(withTableSorting(withTableActions(Table)));
+
+type TableData = {
+    user: string;
+    project: string;
+    status: {
+        theme: LabelProps['theme'];
+        title: string;
+    };
+    updated: string;
+};
+
+const tableColumns: TableColumnConfig<TableData>[] = [
+    {
+        id: 'user',
+        name: 'User',
+        template: ({user}) => {
+            return <User avatar={{imgUrl: avatar1Asset.src, alt: user}} name={user} />;
+        },
+        meta: {
+            sort: true,
+        },
+    },
+    {
+        id: 'project',
+        name: 'Project',
+        width: '100%',
+        meta: {
+            sort: true,
+        },
+    },
+    {
+        id: 'status',
+        name: 'Status',
+        template: ({status}) => {
+            return <Label theme={status.theme}>{status.title}</Label>;
+        },
+    },
+    {
+        id: 'updated',
+        name: 'Updated',
+    },
+];
+
+const data = new Array(10).fill(0).map((_, index) => {
+    return {
+        user: users[(index + 1) % 7],
+        project: projects[(index + 1) % 10],
+        updated: dateTime()
+            .add(Math.round(Math.random() * 30), 'days')
+            .format('DD.MM.YYYY hh:mm'),
+        status: labels[(index + 1) % 4],
+    };
+});
+
+const getRowActions = (): TableAction<Record<string, any>>[] => {
+    return [
+        {
+            text: 'Edit',
+            theme: 'normal',
+            icon: <Pencil />,
+            handler: () => {},
+        },
+        {
+            text: 'Copy',
+            handler: () => {},
+            theme: 'normal',
+            icon: <Copy />,
+        },
+        {
+            text: 'Remove',
+            handler: () => {},
+            icon: <TrashBin />,
+            theme: 'danger',
+        },
+    ];
+};
+
+export const TablePreview = ({justify}: {justify: string}) => {
+    const [tableSelectedIds, setTableSelectedIds] = React.useState(['1']);
+    const [state, setState] = React.useState({page: 1, pageSize: 10, total: 1000});
+
+    const handleUpdate: PaginationProps['onUpdate'] = (page, pageSize) => {
+        setState((prevState) => ({...prevState, page, pageSize}));
+    };
+
+    return (
+        <Flex direction="column" alignItems={justify} gap={4}>
+            <Text variant="header-1">Table</Text>
+            <Flex justifyContent="space-between" width="100%">
+                <Flex gap={2}>
+                    <TextInput placeholder="Search" />
+                    <Select
+                        placeholder="Choose from the list"
+                        options={projects.map((pr) => ({
+                            value: pr,
+                            content: pr,
+                        }))}
+                    />
+                    <Select
+                        placeholder="Choose from the list"
+                        options={labels.map((label) => ({
+                            value: label.title,
+                            content: label.title,
+                        }))}
+                    />
+                </Flex>
+                <Button size="m" view="action">
+                    <Icon data={Plus} />
+                    <Text variant="body-1">Add new user</Text>
+                </Button>
+            </Flex>
+            <SelectionTable
+                className={b('table-preview')}
+                columns={tableColumns as TableColumnConfig<TableDataItem>[]}
+                data={data}
+                selectedIds={tableSelectedIds}
+                getRowActions={getRowActions}
+                onSelectionChange={setTableSelectedIds}
+            />
+            <Pagination {...state} compact={false} onUpdate={handleUpdate} />
+        </Flex>
+    );
+};
---
+++ src/components/Themes/ui/PreviewTab/constants.ts
@@ -0,0 +1,29 @@
+export const labels = [
+    {title: 'In progress', theme: 'info'},
+    {title: 'Open', theme: 'success'},
+    {title: 'Testing', theme: 'unknown'},
+    {title: 'Some mistakes', theme: 'danger'},
+];
+
+export const projects = [
+    'WWCC - incorrect - fix',
+    'ARC - down lux parameters',
+    'ASMR - double',
+    'WWCC - store',
+    'LUX - programming',
+    'DSLL - second screen',
+    'ARC - exstamina',
+    'ASMR - loader connections',
+    'LUX - check',
+    'DSLL - second screen with zoom',
+];
+
+export const users = [
+    'John Smith',
+    'David Jones',
+    'Michael Johnson',
+    'William Wilson',
+    'Charles Carter',
+    'Robert Rodriguez',
+    'Alexander Adams',
+];
