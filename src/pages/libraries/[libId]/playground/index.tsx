8de6becb6f4d2a8d9c5181e8a9a3b82a1c1b95b4
makhnatkin <makhnatkin@yandex-team.ru>
2024-09-25 19:34:20 +0200

    fix(editor): fixed playground getStaticPaths

--- src/pages/libraries/[libId]/playground/index.tsx
+++ src/pages/libraries/[libId]/playground/index.tsx
@@ -10,10 +10,20 @@ import {getLibsList} from '../../../../utils';
 const libs = getLibsList();
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = libs
+        .filter((libItem) => availablePlaygrounds.includes(libItem.config.id))
+        .map((libItem) => ({
+            params: {libId: libItem.config.id},
+        }));
+
     return {
-        paths: libs
-            .filter((lib) => availablePlaygrounds.includes(lib.config.id))
-            .map((item) => ({params: {libId: item.config.id}})),
+        paths: paths.length
+            ? paths
+            : [
+                  {
+                      params: {libId: ''},
+                  },
+              ],
         fallback: false,
     };
 };
