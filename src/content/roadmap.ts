10cb79c42bf3c65b16182ce810d1b6615730831a
vvtimofeev <108340247+vvtimofeev@users.noreply.github.com>
2025-01-30 13:01:11 +0300

    feat: roadmap 2025 (#330)

--- src/content/roadmap.ts
+++ src/content/roadmap.ts
@@ -5,46 +5,18 @@ import {RoadmapTask, RoadmapTaskStatus} from '../components/Roadmap';
 export const getRoadmapTasks = (t: TFunction): RoadmapTask[] => [
     {
         title: t('home:roadmap_items_item1'),
-        status: RoadmapTaskStatus.Completed,
+        status: RoadmapTaskStatus.InProgress,
     },
     {
         title: t('home:roadmap_items_item2'),
-        status: RoadmapTaskStatus.Completed,
+        status: RoadmapTaskStatus.InProgress,
     },
     {
         title: t('home:roadmap_items_item3'),
-        status: RoadmapTaskStatus.InProgress,
+        status: RoadmapTaskStatus.Planned,
     },
     {
         title: t('home:roadmap_items_item4'),
-        status: RoadmapTaskStatus.InProgress,
-    },
-    {
-        title: t('home:roadmap_items_item5'),
-        status: RoadmapTaskStatus.InProgress,
-    },
-    {
-        title: t('home:roadmap_items_item6'),
-        status: RoadmapTaskStatus.Completed,
-    },
-    {
-        title: t('home:roadmap_items_item7'),
-        status: RoadmapTaskStatus.InProgress,
-    },
-    {
-        title: t('home:roadmap_items_item8'),
-        status: RoadmapTaskStatus.Planned,
-    },
-    {
-        title: t('home:roadmap_items_item9'),
-        status: RoadmapTaskStatus.Planned,
-    },
-    {
-        title: t('home:roadmap_items_item10'),
-        status: RoadmapTaskStatus.Planned,
-    },
-    {
-        title: t('home:roadmap_items_item11'),
         status: RoadmapTaskStatus.Planned,
     },
 ];
