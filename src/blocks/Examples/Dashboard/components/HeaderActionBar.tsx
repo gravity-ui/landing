afeb0e09429bf8e67f1b25e4c56f2af5e4eae719
saxumcordis <victor123952@gmail.com>
2025-03-14 23:14:26 +0300

    add avatars

--- src/blocks/Examples/Dashboard/components/HeaderActionBar.tsx
+++ src/blocks/Examples/Dashboard/components/HeaderActionBar.tsx
@@ -1,7 +1,11 @@
-import {ArrowUpFromSquare, Bell, Magnifier, Person, Persons, Plus} from '@gravity-ui/icons';
+import {ArrowUpFromSquare, Bell, Magnifier, Plus} from '@gravity-ui/icons';
 import {ActionBar} from '@gravity-ui/navigation';
 import {Avatar, AvatarStack, Button, Hotkey, Icon, TextInput} from '@gravity-ui/uikit';
 import {block} from 'src/utils';
+
+import avatar1 from '../avatars/avatar1.png';
+import avatar2 from '../avatars/avatar2.png';
+import avatar3 from '../avatars/avatar3.png';
 
 import './HeaderActionBar.scss';
 
@@ -26,9 +30,9 @@ export const HeaderActionBar = () => {
                 <ActionBar.Group>
                     <ActionBar.Item className={b('centered', {margin: true})}>
                         <AvatarStack className={b('users')} max={3}>
-                            <Avatar icon={Person} />
-                            <Avatar icon={Persons} />
-                            <Avatar icon={Person} />
+                            <Avatar imgUrl={avatar1.src} />
+                            <Avatar imgUrl={avatar2.src} />
+                            <Avatar imgUrl={avatar3.src} />
                         </AvatarStack>
                         <Button size="m" view="outlined">
                             <Icon size={16} data={Plus} />
