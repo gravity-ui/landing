9f225ecbc0db3eebb9befa23004cf3e5e5cbb571
vvtimofeev <108340247+vvtimofeev@users.noreply.github.com>
2025-03-25 12:27:57 +0300

    Add 2 news topics (#360)
    * feat: add news
    
    * fix: change dates
    
    * fix: correct typo

--- src/content/landing.ts
+++ src/content/landing.ts
@@ -62,15 +62,15 @@ export const getLanding = (t: TFunction): CustomPageContent => ({
                 title: t('home:news_title'),
                 items: [
                     {
-                        date: '2025-02-21T11:00:00.000Z',
+                        date: '2025-03-25T09:00:00.000Z',
                         content: t('home:news_items_item1'),
                     },
                     {
-                        date: '2025-02-14T16:00:00.000Z',
+                        date: '2025-03-24T19:00:00.000Z',
                         content: t('home:news_items_item2'),
                     },
                     {
-                        date: '2025-02-06T14:00:00.000Z',
+                        date: '2025-02-21T11:00:00.000Z',
                         content: t('home:news_items_item3'),
                     },
                 ],
@@ -80,13 +80,13 @@ export const getLanding = (t: TFunction): CustomPageContent => ({
             //     href: 'https://forms.yandex.ru/surveys/13733147.8993c47ecb24841f51ca79b039326a5ce301538e',
             // },
         },
-        {
-            type: CustomBlock.Iframe,
-            width: 560,
-            height: 315,
-            src: 'https://runtime.strm.yandex.ru/player/video/vplvibavcepgpr3wkjew?autoplay=0&mute=1',
-            allow: 'autoplay; fullscreen; accelerometer; gyroscope; picture-in-picture; encrypted-media',
-        },
+        // {
+        //     type: CustomBlock.Iframe,
+        //     width: 560,
+        //     height: 315,
+        //     src: 'https://runtime.strm.yandex.ru/player/video/vplvibavcepgpr3wkjew?autoplay=0&mute=1',
+        //     allow: 'autoplay; fullscreen; accelerometer; gyroscope; picture-in-picture; encrypted-media',
+        // },
         {
             type: CustomBlock.Libraries,
             backgroundColor: 'rgba(37, 27, 37, 0.5)',
