3ec93b9b82ebf894eaa59e00075f1e4f8e5880e7
Kirill Revenkov <revenkov-k@yandex-team.ru>
2025-04-15 18:46:06 +0300

    feat: add action group

--- src/components/Themes/ui/PreviewTab/MailPreview/CurrentEmail/CurrentEmail.tsx
+++ src/components/Themes/ui/PreviewTab/MailPreview/CurrentEmail/CurrentEmail.tsx
@@ -1,5 +1,48 @@
-import {Flex} from '@gravity-ui/uikit';
+import {
+    ArrowShapeTurnUpRight,
+    ArrowUturnCcwLeft,
+    ArrowUturnCwRight,
+    Clock,
+    Ellipsis,
+    Folder,
+    LayoutTabs,
+    TrashBin,
+} from '@gravity-ui/icons';
+import {Divider, Flex} from '@gravity-ui/uikit';
+import {block} from 'src/utils';
+
+import {Header} from '../Header/Header';
+import {getBlockName} from '../cn';
+
+import {ActionGroup} from './ActionGroup/ActionGroup';
+
+const b = block(getBlockName('current-email'));
+
+const LEFT_ACTIONS = [
+    {icon: <Folder />},
+    {icon: <LayoutTabs />},
+    {icon: <TrashBin />},
+    {icon: <Clock />},
+];
+const RIGHT_ACTIONS = [
+    {icon: <ArrowUturnCcwLeft />},
+    {icon: <ArrowShapeTurnUpRight />},
+    {icon: <ArrowUturnCwRight />},
+    {icon: <Ellipsis />},
+];
+
+const ActionBar = () => (
+    <Header>
+        <ActionGroup actions={LEFT_ACTIONS} />
+        <ActionGroup actions={RIGHT_ACTIONS} />
+    </Header>
+);
 
 export const CurrentEmail = () => {
-    return <Flex direction="column" width={488}></Flex>;
+    return (
+        <Flex direction="column" className={b()}>
+            <ActionBar />
+            <Divider />
+        </Flex>
+    );
 };
