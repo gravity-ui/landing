ac28ce84c1de4a25c3533382961c0f3beebff5cf
aobityutskiy <aobityutskiy@yandex-team.ru>
2025-04-17 19:23:47 +0300

    feat: add examples for stepper and breadcrumbs with custom icon

--- src/content/components/uikit/examples/components/index.ts
+++ src/content/components/uikit/examples/components/index.ts
@@ -2,3 +2,5 @@ export * from './ArrowToggleExample';
 export * from './PopupAnchorExample';
 export * from './PopupPlacementExample';
 export * from './SliderExample';
+export * from './StepperCustomIconExample';
+export * from './BreadcrumbsCustomIconExample';
