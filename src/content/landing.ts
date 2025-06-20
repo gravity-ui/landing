efe0a59505ddab7f177f17c4a9383f903e47217a
vvtimofeev <108340247+vvtimofeev@users.noreply.github.com>
2025-06-20 17:28:27 +0300

    docs: add two new topics to news (#400)

--- src/content/landing.ts
+++ src/content/landing.ts
@@ -59,15 +59,15 @@ export const getLanding = (t: TFunction): CustomPageContent => ({
                 title: t('home:news_title'),
                 items: [
                     {
-                        date: '2025-05-29T14:00:00.000Z',
+                        date: '2025-06-20T14:00:00.000Z',
                         content: t('home:news_items_item1'),
                     },
                     {
-                        date: '2025-05-28T14:00:00.000Z',
+                        date: '2025-06-19T14:00:00.000Z',
                         content: t('home:news_items_item2'),
                     },
                     {
-                        date: '2025-04-09T14:00:00.000Z',
+                        date: '2025-05-29T14:00:00.000Z',
                         content: t('home:news_items_item3'),
                     },
                 ],
