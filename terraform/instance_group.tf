c0a4864dc56effc51336c55e96199f95facaf629
3Tn0 <v.vl.stepanenko@gmail.com>
2025-07-03 20:19:58 +0300

    Add deploy

--- terraform/instance_group.tf
+++ terraform/instance_group.tf
@@ -6,16 +6,16 @@ resource "yandex_compute_instance_group" "landing_ig" {
   name               = "landing-instance-group"
   folder_id          = var.yc_folder_id
   service_account_id = yandex_iam_service_account.ig_sa.id
-  
+
   instance_template {
     platform_id = "standard-v3"
-    
+
     resources {
       cores         = var.vm_resources.cores
       memory        = var.vm_resources.memory
       core_fraction = 100
     }
-    
+
     boot_disk {
       mode = "READ_WRITE"
       initialize_params {
@@ -23,10 +23,10 @@ resource "yandex_compute_instance_group" "landing_ig" {
         size     = var.vm_resources.disk
       }
     }
-    
+
     network_interface {
-      network_id         = yandex_vpc_network.network.id
-      subnet_ids         = [
+      network_id = yandex_vpc_network.network.id
+      subnet_ids = [
         yandex_vpc_subnet.subnet_a.id,
         yandex_vpc_subnet.subnet_b.id,
         yandex_vpc_subnet.subnet_d.id
@@ -34,7 +34,7 @@ resource "yandex_compute_instance_group" "landing_ig" {
       nat                = true
       security_group_ids = [yandex_vpc_security_group.landing_sg.id]
     }
-    
+
     metadata = {
       docker-container-declaration = yamlencode({
         spec = {
@@ -73,22 +73,22 @@ resource "yandex_compute_instance_group" "landing_ig" {
       EOT
     }
   }
-  
+
   scale_policy {
     fixed_scale {
       size = 3
     }
   }
-  
+
   allocation_policy {
     zones = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
   }
-  
+
   deploy_policy {
     max_unavailable = 1
     max_expansion   = 1
   }
-  
+
   health_check {
     http_options {
       port = 3000
@@ -99,7 +99,7 @@ resource "yandex_compute_instance_group" "landing_ig" {
     healthy_threshold   = 2
     unhealthy_threshold = 3
   }
-  
+
   load_balancer {
     target_group_name = "landing-target-group"
   }
