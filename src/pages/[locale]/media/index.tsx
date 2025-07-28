329e4b6683e50cdc5e7fb7693ea5e0e5ead9b9c7
vivalaakam <vivalaakam@yandex-team.ru>
2025-07-28 16:59:19 +0300

    remove unused function

--- src/pages/[locale]/media/index.tsx
+++ src/pages/[locale]/media/index.tsx
@@ -1,15 +1,8 @@
-import {GetStaticPaths, GetStaticProps} from 'next';
+import {GetStaticProps} from 'next';
 import {useTranslation} from 'react-i18next';
 import {Layout} from 'src/components/Layout/Layout';
 import {Media} from 'src/components/Media/Media';
-import {getI18nPaths, getI18nProps} from 'src/utils';
-
-export const getStaticPaths: GetStaticPaths = async () => {
-    return {
-        paths: getI18nPaths(),
-        fallback: false,
-    };
-};
+import {getI18nProps} from 'src/utils';
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
     const result = {
