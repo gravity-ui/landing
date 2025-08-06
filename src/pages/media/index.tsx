cd0c5e41e99caa50de95b197fe9e98e7e90be5f8
vivalaakam <vivalaakam@yandex-team.ru>
2025-08-06 12:05:45 +0300

    fix rview

--- src/pages/media/index.tsx
+++ src/pages/media/index.tsx
@@ -1,1 +1,22 @@
-export {default, getServerSideProps} from '../[locale]/media';
+import {GetServerSideProps} from 'next';
+import {useTranslation} from 'react-i18next';
+import {Layout} from 'src/components/Layout/Layout';
+import {Media} from 'src/components/Media/Media';
+import {getI18nProps} from 'src/utils';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    return {
+        props: {
+            ...(await getI18nProps(ctx)),
+        },
+    };
+};
+
+export default function MediaPage() {
+    const {t} = useTranslation();
+    return (
+        <Layout title={t('media:title')} isPageConstructor>
+            <Media />
+        </Layout>
+    );
+}
