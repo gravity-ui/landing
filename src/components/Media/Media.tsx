d35f04a4e9478982a0f4e6f799f9f30dd0454fc2
vivalaakam <vivalaakam@yandex-team.ru>
2025-08-06 11:33:32 +0300

    fix imports

--- src/components/Media/Media.tsx
+++ src/components/Media/Media.tsx
@@ -3,16 +3,25 @@ import {useTranslation} from 'next-i18next';
 import {useMemo} from 'react';
 
 import contentEn from '../../content/media-en.yaml';
+import contentEs from '../../content/media-es.yaml';
 import contentRu from '../../content/media-ru.yaml';
+import contentZh from '../../content/media-zh.yaml';
 
 export const Media = () => {
     const {t, i18n} = useTranslation();
 
     const content = useMemo(() => {
-        if (i18n.language === 'ru') {
-            return contentRu;
+        switch (i18n.language) {
+            case 'ru':
+                return contentRu;
+            case 'es':
+                return contentEs;
+            case 'zh':
+                return contentZh;
+            case 'en':
+            default:
+                return contentEn;
         }
-        return contentEn;
     }, [i18n.language, t]);
 
     return <PageConstructor content={content as PageContent} />;
