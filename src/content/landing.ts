0219d56db6340e4dd5ba383624ddbbc50b9e50fa
vvtimofeev <108340247+vvtimofeev@users.noreply.github.com>
2025-09-02 18:46:32 +0300

    feat: added topic about import in themer (#441)

--- src/content/landing.ts
+++ src/content/landing.ts
@@ -73,15 +73,15 @@ export const getLanding = ({
                 title: t('home:news_title'),
                 items: [
                     {
-                        date: '2025-08-14T14:00:00.000Z',
+                        date: '2025-09-02T14:00:00.000Z',
                         content: t('home:news_items_item1'),
                     },
                     {
-                        date: '2025-08-11T14:00:00.000Z',
+                        date: '2025-08-14T14:00:00.000Z',
                         content: t('home:news_items_item2'),
                     },
                     {
-                        date: '2025-07-30T14:00:00.000Z',
+                        date: '2025-08-11T14:00:00.000Z',
                         content: t('home:news_items_item3'),
                     },
                 ],
