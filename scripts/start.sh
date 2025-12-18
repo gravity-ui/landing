ee8d170120e6179925618033d5971e6b2f86ceae
dgaponov <dagaponov@yandex-team.ru>
2025-12-18 14:56:25 +0300

    feat: add openai keys to start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,10 +12,14 @@ SECRET_ID="e6q2afdufj7scuv328to"
 GITHUB_APP_ID=$(yc lockbox payload get --id $SECRET_ID --key app_id)
 GITHUB_APP_INSTALLATION_ID=$(yc lockbox payload get --id $SECRET_ID --key installation_id)
 GITHUB_APP_PRIVATE_KEY=$(yc lockbox payload get --id $SECRET_ID --key private_key)
+OPENAI_API_KEY=$(yc lockbox payload get --id $SECRET_ID --key openai_api_key)
+OPENAI_MODEL=$(yc lockbox payload get --id $SECRET_ID --key openai_model)
 
 export GITHUB_APP_ID
 export GITHUB_APP_INSTALLATION_ID
 export GITHUB_APP_PRIVATE_KEY
+export OPENAI_API_KEY
+export OPENAI_MODEL
 
 echo "Starting server..."
 exec node server.js
