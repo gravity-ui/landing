c7de9fdb28f48c921f26f13544b5ac9ea5ee1dfe
ngorin <ngorin@yandex-team.ru>
2025-12-25 16:11:43 +0300

    feat: add real mock data in blog

---
+++ src/pages/blog/constants.ts
@@ -0,0 +1,15 @@
+import {Lang} from '@gravity-ui/uikit';
+
+export const localeEn = {
+    code: 'en-En',
+    lang: Lang.En,
+    langName: 'English',
+    pathPrefix: 'en',
+};
+
+export const localeRu = {
+    code: 'ru-Ru',
+    lang: Lang.Ru,
+    langName: 'Russia',
+    pathPrefix: 'ru',
+};
