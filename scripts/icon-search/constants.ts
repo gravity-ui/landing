1cdc8df5afb6fffc63b00b08cc9c02dd8c5a79ae
Daniil Gaponov <dagaponov@yandex-team.ru>
2026-02-11 18:08:49 +0300

    feat: add ci workflow

--- scripts/icon-search/constants.ts
+++ scripts/icon-search/constants.ts
@@ -4,7 +4,9 @@ export const REPO_ROOT = path.resolve(__dirname, '..', '..');
 export const ICONS_LIBRARY_DIR = path.join(REPO_ROOT, 'node_modules', '@gravity-ui', 'icons');
 export const ICONS_LIBRARY_METADATA_PATH = path.join(ICONS_LIBRARY_DIR, 'metadata.json');
 
-export const RENDERED_ICONS_DIR = path.join(__dirname, 'data', 'rendered_icons');
+export const DATA_DIR = path.join(__dirname, 'data');
+export const RENDERED_ICONS_DIR = path.join(DATA_DIR, 'rendered_icons');
+export const ICONS_VERSION_PATH = path.join(DATA_DIR, 'icons_version.txt');
 export const EMBEDDINGS_JSON_PATH = path.join(
     REPO_ROOT,
     'public',
