704b1d50865797f9b32f9b31cc54593a53e31fb0
Vladimir Stepanenko 🏴‍☠️ <endurance@yandex-team.ru>
2026-03-13 15:21:05 +0300

    chore(deploy): fix deploy after nat disabling (#612)

--- terraform/network.tf
+++ terraform/network.tf
@@ -2,11 +2,27 @@ resource "yandex_vpc_network" "network" {
   name = "landing-network"
 }
 
+resource "yandex_vpc_gateway" "nat_gateway" {
+  name = "landing-nat-gateway"
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "rt" {
+  name       = "landing-route-table"
+  network_id = yandex_vpc_network.network.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat_gateway.id
+  }
+}
+
 resource "yandex_vpc_subnet" "subnet_a" {
   name           = "landing-subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.1.0.0/24"]
+  route_table_id = yandex_vpc_route_table.rt.id
 }
 
 resource "yandex_vpc_subnet" "subnet_b" {
@@ -14,6 +30,7 @@ resource "yandex_vpc_subnet" "subnet_b" {
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.2.0.0/24"]
+  route_table_id = yandex_vpc_route_table.rt.id
 }
 
 resource "yandex_vpc_subnet" "subnet_d" {
@@ -21,6 +38,7 @@ resource "yandex_vpc_subnet" "subnet_d" {
   zone           = "ru-central1-d"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.3.0.0/24"]
+  route_table_id = yandex_vpc_route_table.rt.id
 }
 
 resource "yandex_vpc_security_group" "landing_sg" {
