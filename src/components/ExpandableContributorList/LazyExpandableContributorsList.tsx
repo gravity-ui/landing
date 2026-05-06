4e39d713eeb06365b6dc3b3af8643f3e812a46e3
timofeyevvv <timofeyevvv@yandex-team.ru>
2026-05-06 10:15:52 +0300

    fix: padding

--- src/components/ExpandableContributorList/LazyExpandableContributorsList.tsx
+++ src/components/ExpandableContributorList/LazyExpandableContributorsList.tsx
@@ -19,11 +19,20 @@ const getComponent = async () => {
     return (await import('./ExpandableContributorList')).ExpandableContributorList;
 };
 
+function shuffle<T>(arr: T[]): T[] {
+    const result = [...arr];
+    for (let i = result.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [result[i], result[j]] = [result[j], result[i]];
+    }
+    return result;
+}
+
 const getComponentProps = async () => {
     const contributors = await ClientApi.instance.fetchAllContributors();
     const newcomers = contributors.filter((c) => c.isNewcomer);
 
-    return {contributors, newcomers};
+    return {contributors: shuffle(contributors), newcomers};
 };
 
 export const LazyExpandableContributorsList: React.FC<Props> = (props) => {
