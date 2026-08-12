1ec70241da7c7276f0764567cff491a84c351df5
Daniil Gaponov <dagaponov@yandex-team.ru>
2026-08-12 18:13:16 +0300

    feat: derive component catalog from exact package tags
    Co-authored-by: pontiac358 <pontiac358@gmail.com>

--- src/content/components/navigation/index.ts
+++ src/content/components/navigation/index.ts
@@ -1,6 +1,8 @@
 import sortBy from 'lodash/sortBy';
 
 import {getLibConfigById} from '../../../libs/config';
+import {Repos} from '../../../types/common';
+import {getPublishedComponentConfigs, mergeComponentConfigs} from '../catalog';
 import {Component, Lib} from '../types';
 
 import {actionBarConfig} from './ActionBar';
@@ -11,10 +13,9 @@ import {footerConfig} from './Footer';
 import {hotkeysPanelConfig} from './HotkeysPanel';
 import {mobileHeaderConfig} from './MobileHeader';
 import {settingsConfig} from './Settings';
-
 const config = getLibConfigById('navigation');
 
-const components: Component[] = [
+const manualComponents: Component[] = [
     actionBarConfig,
     allPagesPanelConfig,
     asideHeaderConfig,
@@ -24,6 +25,11 @@ const components: Component[] = [
     settingsConfig,
     footerConfig,
 ];
+
+const components = mergeComponentConfigs(
+    getPublishedComponentConfigs(Repos.Navigation),
+    manualComponents,
+);
 
 export const navigationComponents: Lib = {
     id: config.id,
